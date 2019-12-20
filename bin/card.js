d977d24d5f647d0d9d56371aa89ba95aa07eb617
dyyyl <hey@dyyyl.rocks>
2019-12-20 16:56:37 +0100

    updoots

--- bin/card.js
+++ bin/card.js
@@ -9,14 +9,14 @@ const chalk = require("chalk");
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: "round",
+  borderStyle: "round"
 };
 
-const cardText = chalk`{hex('#FFEFD5') Dyl Reynard /} {hex('#00BFFF') @dyyyl}
+const cardText = chalk`{hex('#FFEFD5') Dyl Lavoie /} {hex('#00BFFF') @dyyyl}
 
-     {hex('#FFEFD5').bold Work: } {hex('#FFEFD5') Wandering Web Engineer}
+     {hex('#FFEFD5').bold Work: } {hex('#FFEFD5') Design Systems & A11y}
+    {hex('#FFEFD5').bold Email: } {hex('#00BFFF') hey@dyyyl.rocks}
       {hex('#FFEFD5').bold NPM: } {hex('#00BFFF') https://www.npmjs.com/~dyyyl}
-  {hex('#FFEFD5').bold Twitter: } {hex('#00BFFF') https://twitter.com/dyyyyyyyyyl}
    {hex('#FFEFD5').bold Github: } {hex('#00BFFF') https://github.com/dyyyl}
  {hex('#FFEFD5').bold LinkedIn: } {hex('#00BFFF') https://www.linkedin.com/in/dyyyl/}
       {hex('#FFEFD5').bold Web: } {hex('#00BFFF') https://dyyyl.rocks}
